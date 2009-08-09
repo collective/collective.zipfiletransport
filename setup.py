7ff3e5dcf24a78cdd1bee9920c0c4ae1db914b9c
David Ray <david@enpraxis.net>
2009-08-09 15:36:38 +0000

    revving to final

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ __version__   = '$ Revision 0.0 $'[11:-2]
 from setuptools import setup, find_packages
 import os
 
-version = '2.1.3-rc2'
+version = '2.1.3-final'
 
 setup(name='collective.zipfiletransport',
       version=version,

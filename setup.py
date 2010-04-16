30388f44fefd7e85cac76b03d65571fc4d2f9b90
JC Brand <jc@opkode.com>
2010-04-16 10:58:47 +0000

    Update the setup.py to take the new 'src' directory into account

--- setup.py
+++ setup.py
@@ -25,32 +25,35 @@ import os
 
 version = '2.2.1-final'
 
-setup(name='collective.zipfiletransport',
-      version=version,
-      description="This tool is used to import and export zip files.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read() +
-                       open(os.path.join("docs", "CONTRIBUTORS.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='zipfile bulk upload',
-      author='enPraxis',
-      author_email='info@enpraxis.net',
-      url='http://plone.org/products/zipfiletransport',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-      ],
-      entry_points="""
-        [z3c.autoinclude.plugin]
-        target = plone
-      """,
-      )
+setup(
+    name='collective.zipfiletransport',
+    version=version,
+    description="This tool is used to import and export zip files.",
+    long_description=open("README.txt").read() + "\n" +
+                    open(os.path.join("docs", "HISTORY.txt")).read() +
+                    open(os.path.join("docs", "CONTRIBUTORS.txt")).read(),
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+    "Framework :: Plone",
+    "Programming Language :: Python",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='zipfile bulk upload',
+    author='enPraxis',
+    author_email='info@enpraxis.net',
+    url='http://plone.org/products/zipfiletransport',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    package_dir = {'' : 'src'},
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+    )
+

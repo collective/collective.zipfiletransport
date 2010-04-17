56fcbcfd3d20a0262516c57afd3831e589078d1f
JC Brand <jc@opkode.com>
2010-04-17 07:45:22 +0000

    Bump version to *-dev and add to HISTORY

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ __version__   = '$ Revision 0.0 $'[11:-2]
 from setuptools import setup, find_packages
 import os
 
-version = '2.2.1-final'
+version = '2.2.2-dev'
 
 setup(name='collective.zipfiletransport',
       version=version,

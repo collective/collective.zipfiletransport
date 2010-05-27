bee34599a3d9afed84c37d28d2b5280583a83611
David Ray <david@enpraxis.net>
2010-05-27 17:15:40 +0000

    checking in, retagging 2.2.2 in prep for tag, thanks jcbrand for Plone 4 compatibility

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ __version__   = '$ Revision 0.0 $'[11:-2]
 from setuptools import setup, find_packages
 import os
 
-version = '2.2.2-dev'
+version = '2.2.2ß'
 
 setup(name='collective.zipfiletransport',
       version=version,

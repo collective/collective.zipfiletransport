e1088e208e2aa7bc5c49b2ca70798694b4236094
zopyx <yet@gmx.de>
2012-06-25 05:33:42 +0000

    fixed ZCML deprecation warnings

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ __version__   = '$ Revision 0.0 $'[11:-2]
 from setuptools import setup, find_packages
 import os
 
-version = '3.2.0'
+version = '3.2.1'
 
 setup(name='collective.zipfiletransport',
       version=version,

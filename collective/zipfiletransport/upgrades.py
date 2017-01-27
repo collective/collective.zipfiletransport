b50c438e7a9e4fe6bdcdd2db33c51c792d852084
Thomas Desvenain <thomas.desvenain@gmail.com>
2017-01-27 14:48:22 +0100

    zip64 activation is configured through panel

---
+++ collective/zipfiletransport/upgrades.py
@@ -0,0 +1,7 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def upgrade_100_to_101(context):
+    zipfile_properties = getToolByName(context, 'portal_properties').zipfile_properties
+    zipfile_properties.manage_addProperty(id='allow_zip64', value=False, type=bool)
+

129aab51b3a952d41cbab5203ae2f4a5152baaba
Thomas Desvenain <thomas.desvenain@gmail.com>
2017-01-30 19:19:44 +0100

    Fix upgrade add property

--- collective/zipfiletransport/upgrades.py
+++ collective/zipfiletransport/upgrades.py
@@ -3,5 +3,5 @@ from Products.CMFCore.utils import getToolByName
 
 def upgrade_100_to_101(context):
     zipfile_properties = getToolByName(context, 'portal_properties').zipfile_properties
-    zipfile_properties.manage_addProperty(id='allow_zip64', value=False, type=bool)
+    zipfile_properties.manage_addProperty(id='allow_zip64', value=False, type="boolean")
 
